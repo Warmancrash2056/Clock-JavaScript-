3e6ff1eb36688924e964954925cd634750cef5c6
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2022-11-14 20:49:31 -0500

    Version 1.1

---
+++ Clock Script.js
@@ -0,0 +1,30 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    /* Conditional Ternary takes a three operands and checks if the condition is true to the second if not the third value is assigned to the first */
+    /* '0' add a digit for the clock for design purposes. */
+    h = (h < 10) ? "0" + h : h; /* Ternary operator jus reassign to the same value*/
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
