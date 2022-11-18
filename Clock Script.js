e418578b9205a27296c47463f82c0f2b1f8c93cb
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2022-11-17 21:30:57 -0500

    Version 1.2
    Added responsive screen that changes the content sizing to fit the screen space.

--- Clock Script.js
+++ Clock Script.js
@@ -1,8 +1,8 @@
 function showTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
+    var get_exact_time = new Date();
+    var h = get_exact_time.getHours(); // 0 - 23
+    var m = get_exact_time.getMinutes(); // 0 - 59
+    var s = get_exact_time.getSeconds(); // 0 - 59
     var session = "AM";
     
     if(h == 0){
@@ -19,7 +19,7 @@ function showTime(){
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
     
-    var time = h + ":" + m + ":" + s + " " + session;
+    var time = h + ":" + m + ":" + s + "" + session;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
     
